59e58097d9f5e63c9fcb32bde00ffb98204e909b
evansjai <jai.a.evans@gmail.com>
2015-06-28 19:56:27 -0400

    First flavor check and init hubbbub

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import darn.nothing.Play;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Play play= new Play();
+
+        play.didNothing();
+    }
+}
